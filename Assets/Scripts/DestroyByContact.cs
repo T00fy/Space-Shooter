d01dab65897ccd8c77055204eb59947a573ddd5b
T00fy <mostafa.alsari@gmail.com>
2016-02-26 15:25:11 +1300

    Added an asteroid object

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Boundary") {
+            return;
+        }
+        Destroy(other.gameObject); //the bolt or the player
+        Destroy(gameObject); //the asteroid itself
+    }
+}
