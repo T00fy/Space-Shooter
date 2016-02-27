3af6ac45ae5c2edf0b0b46b3aea174e4cee60fc9
T00fy <mostafa.alsari@gmail.com>
2016-02-27 19:46:46 +1300

    Added Scrolling Background

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+    public float scrollSpeed;
+
+    private float tileSizeY;
+    private Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        tileSizeY = transform.localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * -scrollSpeed, tileSizeY); //loops one second increments until it reaches the length of the bg scale (30) then resets back to 1
+        // will return the remainder to the position
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
