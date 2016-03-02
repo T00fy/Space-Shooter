588634788bf8834e3623074d36e2f0301c2cb90b
T00fy <mostafa.alsari@gmail.com>
2016-03-02 18:56:09 +1300

    New Music

---
+++ Assets/Scripts/EasyFadeIn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EasyFadeIn : MonoBehaviour
+{
+    private AudioSource audios;
+    public int approxSecondsToFade = 10;
+    void Start() {
+        audios = GetComponent<AudioSource>();
+
+    }
+    void FixedUpdate()
+    {
+        
+        if (audios.volume < 1)
+        {
+            audios.volume = audios.volume + (Time.deltaTime / (approxSecondsToFade + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
