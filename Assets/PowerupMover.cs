b034efc7230c01fd95016388213127306347915d
T00fy <mostafa.alsari@gmail.com>
2016-03-07 17:16:18 +1300

    Fixed Asteroid Fragmentation

---
+++ Assets/PowerupMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupMover : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -7,13 +7,15 @@ public class DestroyByContact : MonoBehaviour
     public GameObject explosion;
     public GameObject playerExplosion;
     public GameObject[] splitAsteroids;
+    public GameObject[] powerUps;
     public bool fragmentable;
     public int scoreValue;
     private GameController gc;
     private HealthController healthControllerFirstObj;
     private DamageController damageControllerFirstObj;
-    private HealthController healthControllerSecondObj;
-    private DamageController damageControllerSecondObj;
+    private HealthController healthControllerBolt;
+    private DamageController damageControllerBolt;
+    private bool dropPowerUp;
 
     void Start()
     {
@@ -25,6 +27,10 @@ public class DestroyByContact : MonoBehaviour
         else {
             Debug.Log("Can't find gamecontroller");
         }
+        if (powerUps.Length > 0) {
+            dropPowerUp = GetRandomBoolean();
+        }
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -35,33 +41,39 @@ public class DestroyByContact : MonoBehaviour
                 return;
             case "Enemy":
                 return;
+            case "Powerup":
+                return;
         }
+        Debug.Log("Other: " + other.tag);
+        Debug.Log("GO: " + gameObject.tag);
         healthControllerFirstObj = gameObject.GetComponent<HealthController>();
         damageControllerFirstObj = gameObject.GetComponent<DamageController>();
-        healthControllerSecondObj = other.gameObject.GetComponent<HealthController>();
-        damageControllerSecondObj = other.gameObject.GetComponent<DamageController>();
+        healthControllerBolt = other.gameObject.GetComponent<HealthController>();
+        damageControllerBolt = other.gameObject.GetComponent<DamageController>();
         int healthFirstObj = healthControllerFirstObj.GetHealth();
         int damageDealtFirstObj = damageControllerFirstObj.GetDamage();
-        int healthSecondObj = healthControllerSecondObj.GetHealth();
-        int damageDealtSecondObj = damageControllerSecondObj.GetDamage();
-        if (damageDealtSecondObj < healthFirstObj)
+        int healthBolt = healthControllerBolt.GetHealth();
+        int damageDealtBolt = damageControllerBolt.GetDamage();
+        if (damageDealtFirstObj < healthBolt)
         {
-            healthControllerFirstObj.SetHealth(healthFirstObj - damageDealtSecondObj);
-            Debug.Log(healthControllerFirstObj.GetHealth());
+            healthControllerBolt.SetHealth(healthBolt - damageDealtFirstObj);
         }
         else {
-            DestroyFirstObject();
+            DestroyBolt(other);
         }
-        if (damageDealtFirstObj < healthSecondObj) {
-            healthControllerSecondObj.SetHealth(healthSecondObj - damageDealtFirstObj);
+        if (damageDealtBolt < healthFirstObj)
+        {
+            healthControllerFirstObj.SetHealth(healthFirstObj - damageDealtBolt);
+            
         }
         else {
-            DestroySecondObject(other);
+            DestroyEnemy(other);
         }
+
 
     }
 
-    private void DestroySecondObject(Collider other)
+    private void DestroyBolt(Collider other)
     {
         if (other.CompareTag("Player"))
         {
@@ -70,26 +82,13 @@ public class DestroyByContact : MonoBehaviour
         }
 
 
-        for (int i = 0; i < other.transform.childCount; i++) {
-            if (other.transform.GetChild(i).CompareTag("PlayerWeapon"))
-            {
-                gc.AddScore(scoreValue);
-                break;
-            }
-        }
 
-        if (fragmentable)
-        {
-            BreakIntoFragments(other);
-        }
-        else { //the bolt or the player
-
+        Debug.Log("Ges");
             Destroy(other.gameObject);
-        }
     }
 
-    private void DestroyFirstObject()
-    {
+    private void DestroyEnemy(Collider other)
+    { 
         if (CompareTag("Player"))
         {
             Instantiate(playerExplosion, transform.position, transform.rotation);
@@ -101,8 +100,28 @@ public class DestroyByContact : MonoBehaviour
         {
             Instantiate(explosion, transform.position, transform.rotation);
         }
-//        gc.AddScore(scoreValue);
+        if (gameObject.CompareTag("Enemy") && dropPowerUp) {
+            CreatePowerUp();
+        }
+        for (int i = 0; i < other.transform.childCount; i++)
+        {
+            if (other.transform.GetChild(i).CompareTag("PlayerWeapon"))
+            {
+                gc.AddScore(scoreValue);
+                break;
+            }
+        }
+        if (fragmentable)
+        {
+            BreakIntoFragments(other);
+        }
+
         Destroy(gameObject);
+    }
+
+    void CreatePowerUp()
+    {
+        Instantiate(powerUps[Random.Range(0, powerUps.Length)], transform.position, transform.rotation * Random.rotation);
     }
 
     void BreakIntoFragments(Collider other)
@@ -113,18 +132,33 @@ public class DestroyByContact : MonoBehaviour
             fragmentRb.constraints = RigidbodyConstraints.FreezePositionY;
             splitAsteroids[i].tag = "Enemy";
         }
+        int swap = 1;
         for (int i = 0; i < splitAsteroids.Length; i++)
-        { 
-            Instantiate(splitAsteroids[i], transform.position, transform.rotation * Random.rotation);
-        }
-        if (other.CompareTag("Bolt"))
         {
-            Destroy(other.gameObject);
+            swap = swap * -1;
+            Instantiate(splitAsteroids[i], other.transform.position + new Vector3(swap * 1f,0,0), Quaternion.AngleAxis(90, swap * transform.right) * Random.rotation); //Random.rotation
         }
-        if (!gameObject.CompareTag("Laser")) {
-            Destroy(gameObject);
-        }
-        
 
+    }
+
+    bool GetRandomBoolean()
+    {
+    if (Random.value <= 0.15)
+    {
+        return true;
+    }
+    return false;
+    }
+
+    int RandomSign()
+    {
+        int randomSign = Random.Range(0, 2);
+        if (randomSign == 0)
+        {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     }
 }
